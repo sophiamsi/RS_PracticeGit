2b624a50ca56cfd554e93a2cd4fb4df0a212c2b6
Admin <sundaydog1@yandex.ru>
2023-01-08 19:33:11 -0600

    added first class

---
+++ src/tatiana/Tatiana1.java
@@ -0,0 +1,8 @@
+package tatiana;
+
+public class Tatiana1 {
+    public static void main(String[] args) {
+        System.out.println("Hey guys!!!!!");
+        System.out.println("It's Sunday funday y'all!");
+    }
+}
