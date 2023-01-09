5b70a5c587ec6cbee7e5a603f9057e9fddf203c9
makhliyo <mahliyogafurova27@gmail.com>
2023-01-08 19:33:20 -0500

    added Maya's project

---
+++ src/maya/Maya1.java
@@ -0,0 +1,14 @@
+package maya;
+
+public class Maya1 {
+    public static void main(String[] args) {
+
+
+
+
+        // Hello
+        System.out.println("This is Maya");
+        System.out.println("Hello My Team");
+
+    }
+}
